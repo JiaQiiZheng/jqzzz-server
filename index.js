8db3d9d1eeeb7c8ca0736c92234b253c0db8e87d
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 03:24:22 -0500

    last

--- index.js
+++ index.js
@@ -23,6 +23,33 @@ app.use("/auth", authRouter);
 app.use("/", homeRouter);
 app.use(verifyJWT);
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+module.exports = allowCors(handler);
+
 import mongoose, { setDriver } from "mongoose";
 mongoose.set("strictQuery", false);
 const CONNECTION_URL = process.env.DATABASE_URL;
