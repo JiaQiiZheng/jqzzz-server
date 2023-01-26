056c996924bb7eda0102e899968ffd1252688d94
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 01:02:14 -0500

    first

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://www.jqzzz.com",
   "http://localhost:5173",
   "http://localhost:8080",
+  "https://jqzzz-server.vercel.app/",
 ];
 
 export default allowedOrigins;
