9c769dd010babcda83cdbd2bc9900b36805d160b
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 02:19:51 -0500

    first

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
   "https://www.jqzzz.com/",
   "https://jqzzz.com/",
-  "https://jqzzz-client.vercel.app",
+  "https://jqzzz-client.vercel.app/",
   "http://localhost:5173",
+  "https://www.jqzzz.com/auth",
 ];
 
 export default allowedOrigins;
