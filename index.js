5cd7c57b4f875a92b0af0038f133cfbb5375b793
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 01:31:18 -0500

    first

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import corsOptions from "./config/corsOptions.js";
 import credentials from "./middleWare/credentials.js";
+import http from "http";
 // import verifyJWT from "./middleware/verifyJWT.js";
 
 const app = express();
@@ -27,15 +28,14 @@ mongoose.set("strictQuery", false);
 const CONNECTION_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 8080;
 
+const server = http.createServer(app);
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(
-        `Connected to Mongoose, Server Running on Port: http://localhost:${PORT}`
-      )
+    server.listen(PORT, () =>
+      console.log(`Connected to Mongoose, Server listening on port ${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
 
-export default app;
+export default server;
